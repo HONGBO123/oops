9bde290fef49c6f3f0758d0770f357eb5604dab9
klittle <kyler.little@wsu.edu>
2018-01-17 17:54:38 -0800

    start of hw1; parser implement and skeleton bst

---
+++ bst_number_list/BST.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bst_number_list
+{
+    class Node
+    {
+        internal Node Left { get; set; }
+        internal Node Right { get; set; }
+        public static int Data; 
+        public Node(int data)
+        {
+            Data = data;
+            Left = null;      // null reference? Does this make sense? Then I can use "?." operator to get next node
+            Right = null;
+        }
+        // Make this a friend to BST
+
+
+        /*override <, >, !=, ==, Equals, toString
+         */
+    }
+    class BST
+    {
+        private Node head = null;
+        
+
+        public BST(int[] numberList)
+        {
+            foreach (int num in numberList)
+            {
+                
+            }
+        }
+
+        public void PrintInorder()
+        {
+
+        }
+    }
+}
---
+++ bst_number_list/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bst_number_list
+{
+    static class StringParser
+    {
+        /*
+         * ParseToIntArray: 
+         */
+        public static int[] ParseToIntArray(string line)
+        {
+            string[] tokens = line.Split(' ');
+            int[] arr = new int[tokens.Length];
+            for (int i = 0; i < arr.Length; ++i)
+            {
+                try
+                {
+                    int number = int.Parse(tokens[i]);
+                    arr[i] = number;
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("Unable to convert '{0}'.", tokens[i]);
+                }
+                catch (OverflowException)
+                {
+                    Console.WriteLine("'{0}' is out of range of the Int32 type.", tokens[i]);
+                }
+            }
+            return arr;
+        }
+    }
+
+    class BSTProgram
+    {
+        static void Main(string[] args)
+        {
+            BST binarySearchTree = null;
+            do
+            {
+                Console.WriteLine("Enter a collection of numbers in the range [0, 100], separated by spaces:\n");
+                int[] numberList = StringParser.ParseToIntArray(Console.ReadLine());
+                binarySearchTree = new BST(numberList);
+            } while (binarySearchTree != null);      // BST constructor returns null if user entered 0 numbers or number list with duplicates
+
+            // 
+            binarySearchTree.PrintInorder();
+            
+
+        }
+    }
+}
