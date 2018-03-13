68f6836f1a92f4a5dd73335b2d66f24d2a97dab6
klittle <kyler.little@wsu.edu>
2018-03-12 19:08:45 -0700

    beginnings of SpreadSheet Engine DLL

--- Spreadsheet/Spreadsheet/Form1.cs
+++ Spreadsheet/Spreadsheet/Form1.cs
@@ -12,6 +12,19 @@ namespace Spreadsheet
 {
     public partial class SpreadsheetForm : Form
     {
+        /// <summary>
+        /// Constants that affect behavior of the program-- all in one place that is easy to manage.
+        /// </summary>
+        static class Constants
+        {
+            public const int numberOfRows = 50;
+            public const string columnHeaders = "A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z";    // can easily add more space-separated header names
+            public const int gridWidth = 800;
+            public const int gridHeight = 500;
+            public const int formWidth = gridWidth;
+            public const int formHeight = gridHeight + 50;
+        }
+
         public SpreadsheetForm()
         {
             InitializeComponent();
@@ -19,21 +32,27 @@ namespace Spreadsheet
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            SetUpLayout();
-            
+            SetUpFormView();
+            SetUpDataGridView();
+        }
+
+        public void SetUpFormView()
+        {
+            this.Size = new System.Drawing.Size(Constants.formWidth, Constants.formHeight);
         }
 
         /// <summary>
         /// Disallows user from adding/removing rows; initializes columns with headers 'A' to 'Z'
         /// </summary>
-        private void SetUpLayout()
+        private void SetUpDataGridView()
         {
             this.dataGridView1.Columns.Clear();
             this.dataGridView1.AllowUserToAddRows = false;
             this.dataGridView1.AllowUserToDeleteRows = false;
-            for (int i = (int)'A'; i <= (int)'Z'; ++i)
+            this.dataGridView1.Size = new System.Drawing.Size(Constants.gridWidth, Constants.gridHeight);
+            foreach (string colHeader in Constants.columnHeaders.Split(new char[] { ','}))       // split columnHeaders into separate strings by delimiter ','
             {
-                this.dataGridView1.Columns.Add(Convert.ToChar(i).ToString(), Convert.ToChar(i).ToString());
+                this.dataGridView1.Columns.Add(colHeader, colHeader);       // add as column header
             }
         }
     }
---
+++ Spreadsheet/SpreadsheetEngine/Cell.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+
+namespace SpreadsheetEngine
+{
+    public abstract class SpreadsheetCell : INotifyPropertyChanged
+    {
+        /// <summary>
+        /// Fields:
+        ///     _text
+        ///     _value
+        /// </summary>
+        protected string _text = "";
+        protected string _value = "";
+        private int _row_index;
+        private int _col_index;
+
+        /// <summary>
+        /// Constructor for abstract SpreadsheetCell. Can NOT be instantiated.
+        /// </summary>
+        /// <param name="r_index"></param>
+        /// <param name="c_index"></param>
+        public SpreadsheetCell(int r_index, int c_index)
+        {
+            _row_index = r_index; _col_index = c_index;
+        }
+
+        /// <summary>
+        /// Text Property
+        ///     Setter first checks if value is equal to _text. If not, sets and fires PropertyChanged event.
+        /// </summary>
+        public string Text
+        {
+            get
+            {
+                return _text;
+            }
+            set
+            {
+                if (value != _text)
+                {
+                    _text = value;
+                    // FIRE PROPERTY CHANGED EVENT
+                }
+            }
+        }
+
+        /// <summary>
+        /// Value property-- READ ONLY.
+        ///     Represents the "evaluated" value of the cell.
+        /// </summary>
+        public string Value
+        {
+            get
+            {
+                return _value;
+            }
+            protected set     // only classes within this DLL can set; even classes that inherit from SpreadsheetCell outside of this DLL cannot
+            {
+
+            }
+        }
+
+        event PropertyChangedEventHandler INotifyPropertyChanged.PropertyChanged
+        {
+            add
+            {
+                throw new NotImplementedException();
+            }
+
+            remove
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        protected void OnPropertyChanged()
+        {
+
+        }
+    }
+
+    public class Spreadsheet
+    {
+        public Spreadsheet()
+        {
+            
+        }
+    }
+}
