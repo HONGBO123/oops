38b0ac52cd6443b8c73b51a7dbc0290d234a732f
klittle <kyler.little@wsu.edu>
2018-03-12 17:22:24 -0700

    spreadsheet initial commit

---
+++ Spreadsheet/Spreadsheet/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Spreadsheet
+{
+    public partial class SpreadsheetForm : Form
+    {
+        public SpreadsheetForm()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            SetUpLayout();
+            
+        }
+
+        /// <summary>
+        /// Disallows user from adding/removing rows; initializes columns with headers 'A' to 'Z'
+        /// </summary>
+        private void SetUpLayout()
+        {
+            this.dataGridView1.Columns.Clear();
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            for (int i = (int)'A'; i <= (int)'Z'; ++i)
+            {
+                this.dataGridView1.Columns.Add(Convert.ToChar(i).ToString(), Convert.ToChar(i).ToString());
+            }
+        }
+    }
+}
---
+++ Spreadsheet/SpreadsheetEngine/Class1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+
+namespace SpreadsheetEngine
+{
+    public class SpreadsheetCell : INotifyPropertyChanged
+    {
+        public SpreadsheetCell()
+        {
+            
+        }
+
+        event PropertyChangedEventHandler INotifyPropertyChanged.PropertyChanged
+        {
+            add
+            {
+                throw new NotImplementedException();
+            }
+
+            remove
+            {
+                throw new NotImplementedException();
+            }
+        }
+    }
+}
